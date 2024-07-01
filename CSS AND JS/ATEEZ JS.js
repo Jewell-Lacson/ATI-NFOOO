12313514385a6291300fbfd86431d3aabb9091ed
Jewell-Lacson <jewellsean.lacson@cvsu.edu.ph>
2024-07-01 21:32:12 +0800

    Add files via upload

---
+++ CSS AND JS/ATEEZ JS.js
@@ -0,0 +1,77 @@
+
+	function toggleMember(element) {
+  const activeClass = 'active';
+  const members = document.querySelectorAll('.member');
+
+  // Close any open member
+  members.forEach(member => {
+    if (member !== element && member.classList.contains(activeClass)) {
+      member.classList.remove(activeClass);
+    }
+  });
+
+  // Toggle the clicked member
+  element.classList.toggle(activeClass);
+
+  // If the member is already active, smoothly transition back to their place
+  if (element.classList.contains(activeClass)) {
+    setTimeout(() => {
+      element.style.maxHeight = '300px';
+    }, 10);
+  }
+ 
+}
+( function($) {
+  
+  $(document).ready(function() {
+    
+    var s           = $('.slider'),
+        sWrapper    = s.find('.slider-wrapper'),
+        sItem       = s.find('.slide'),
+        btn         = s.find('.slider-link'),
+        sWidth      = sItem.width(),
+        sCount      = sItem.length,
+        slide_date  = s.find('.slide-date'),
+        slide_title = s.find('.slide-title'),
+        slide_text  = s.find('.slide-text'),
+        slide_more  = s.find('.slide-more'),
+        slide_image = s.find('.slide-image img'),
+        sTotalWidth = sCount * sWidth;
+    
+    sWrapper.css('width', sTotalWidth);
+    sWrapper.css('width', sTotalWidth);
+    
+    var clickCount  = 0;
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+
+      if( $(this).hasClass('next') ) {
+        
+        ( clickCount < ( sCount - 1 ) ) ? clickCount++ : clickCount = 0;
+      } else if ( $(this).hasClass('prev') ) {
+        
+        ( clickCount > 0 ) ? clickCount-- : ( clickCount = sCount - 1 );
+      }
+      TweenMax.to(sWrapper, 0.4, {x: '-' + ( sWidth * clickCount ) })
+
+
+      //CONTENT ANIMATIONS
+
+      var fromProperties = {autoAlpha:0, x:'-50', y:'-10'};
+      var toProperties = {autoAlpha:0.8, x:'0', y:'0'};
+
+      TweenLite.fromTo(slide_image, 1, {autoAlpha:0, y:'40'}, {autoAlpha:1, y:'0'});
+      TweenLite.fromTo(slide_date, 0.4, fromProperties, toProperties);
+      TweenLite.fromTo(slide_title, 0.6, fromProperties, toProperties);
+      TweenLite.fromTo(slide_text, 0.8, fromProperties, toProperties);
+      TweenLite.fromTo(slide_more, 1, fromProperties, toProperties);
+
+    });
+          
+  });
+})(jQuery);
+
+$('.overlay').addClass('overlay-blue');
+
+
